84ca8e40da18e7e2845ec4070bc069936665e8e8
Ku-AA-stanley <plasma12563@gmail.com>
2024-07-21 21:29:32 +0800

    init

--- water-spin.c
+++ water-spin.c
@@ -9,11 +9,7 @@ int main()
   {
     if (GetKeyState(VK_XBUTTON1) < 0) // 檢查滑鼠側鍵是否按下，VK_XBUTTON1代表滑鼠的第一個側鍵
     {
-      mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
-      Sleep(200);
-
-      mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
-      Sleep(100);
+      mouse_event(MOUSEEVENTF_MOVE, 800, 0, 0, 0); // 移動滑鼠
     }
     Sleep(1); // 休眠1毫秒，避免無限迴圈過於耗費CPU資源
   }
